eafb81af286dd9215dcf03627cceaf45a17ea686
Aminah Burch <a.j.burch11@gmail.com>
2018-03-05 14:11:41 -0600

    Practiced using structural and attribute directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = "BeanButter";
+  serverCreated = false;
+  servers = [];
 
   constructor() {
     setTimeout(() => {
@@ -20,6 +22,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created. Name is: ' + this.serverName;
   }
 
